9b4ae4070b4016c1a6a21778c7eb75fb3257a6f2
Prasanth <prasanthrubyist@gmail.com>
2019-01-27 00:33:00 +0530

    added spec and configurations

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Content, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  # Validation tests
+  # ensure columns title, author are present before saving
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:author) }
+  it { should validate_presence_of(:published_date) }
 end
